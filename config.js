5d82d0d84903f62498b58b657fc23b68c3f02c54
Jiajun Xu <143870170+GeorginHsu@users.noreply.github.com>
2025-09-07 03:49:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Point this to your server's FastAPI base
 // e.g. http://your.server.ip:8060  (HTTP) or https://your.domain (HTTPS)
-window.BACKEND_BASE = "https://9290881917b87d.lhr.life";
+window.BACKEND_BASE = "https://21e2b90399e3d4.lhr.life";

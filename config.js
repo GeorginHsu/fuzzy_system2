07d0653c4303b7f67198337a53a75529fbda2054
GeorginHsu <2929991994@qq.com>
2025-09-06 20:32:54 -0700

    change

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Point this to your server's FastAPI base
 // e.g. http://your.server.ip:8060  (HTTP) or https://your.domain (HTTPS)
-window.BACKEND_BASE = "https://c8bb589f9b082a.lhr.life";
+window.BACKEND_BASE = "https://ec14524a580415.lhr.life";
